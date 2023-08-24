d72b331bf7613f0273ece4ebe0bb2ff4a7afe8d6
Metin <meto260@gmail.com>
2023-08-24 20:24:52 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+var result1 = new { naME = "adi", suRName = "soyadi", age = 2 }.Map<Person>();
+Console.WriteLine(JsonSerializer.Serialize(result1));
+
+var result2 = new { naME = "adi", suRName = "soyadi", age = 2 }.Create();
+Console.WriteLine(JsonSerializer.Serialize(result2));
+
+Console.Read();
+
+class Person {
+    public string Name { get; set; }
+    public string SurName { get; set; }
+}
---
+++ MyAutoMapper/MyAutoMapper.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+
+public static class MyAutoMapper {
+    /// <summary>
+    /// Source object convert to target object
+    /// </summary>
+    /// <typeparam name="T">Target type</typeparam>
+    /// <param name="source">Source object</param>
+    /// <exception cref="NullReferenceException">Source object cannot be null</exception>
+    public static T Map<T>(this object source) where T : class, new() {
+        if (source == null)
+            throw new NullReferenceException("'MyAutoMapper.Map' source should be declarated");
+
+        var TargetProps = typeof(T)
+            .GetProperties()
+            .Where(pi => pi.CanWrite && !pi.GetIndexParameters().Any())
+            .ToDictionary(pi => pi.Name.ToLower());
+
+        T obj = new T();
+        foreach (var sprop in source.GetType().GetProperties()) {
+            string name = sprop.Name.ToLower();
+            dynamic val = GetPropValue(source, name);
+            PropertyInfo prop = null;
+            if (val != null &&
+                TargetProps.TryGetValue(name, out prop) &&
+                prop?.PropertyType.IsAssignableFrom(val.GetType())) {
+                prop?.SetValue(obj, val);
+            }
+        }
+        return obj;
+    }
+
+    /// <summary>
+    /// Create a dictionary object from source object
+    /// </summary>
+    /// <param name="source">Source object</param>
+    /// <exception cref="NullReferenceException">Source object cannot be null</exception>
+    public static Dictionary<string, dynamic> Create(this object source) {
+        if (source == null)
+            throw new NullReferenceException("'MyAutoMapper.Create' source should be declarated");
+
+        var instance = new Dictionary<string, dynamic>();
+        foreach (var sprop in source.GetType().GetProperties()) {
+            string name = sprop.Name.ToLower();
+            dynamic val = GetPropValue(source, name);
+            instance.Add(name, val);
+        }
+        return instance;
+    }
+
+
+    private static object GetPropValue(object source, string propertyName) {
+        var property = source.GetType().GetRuntimeProperties().FirstOrDefault(p => string.Equals(p.Name, propertyName, StringComparison.OrdinalIgnoreCase));
+        return property?.GetValue(source);
+    }
+}
